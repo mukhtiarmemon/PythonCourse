b596a72a825b5355be26bdbafab9dacda6aa1c34
mukhtiarmemon <mukhtiar001@yahoo.com>
2024-10-24 12:42:21 +0500

    233

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
+import random
+
+#the function
 def get_choices():
     player_choice = input("Enter Choice (rock, paper, scissors)")
-    computer_choice = "paper"
+    options = ["rock", "paper", "scissors"]
+
+    computer_choice = random.choice(options)    
     choices = {"player": player_choice, "computer": computer_choice}
     
     return choices
+
+#another function
+def check_win(player, computer):
+    return [player, computer]
+
+response_win = check_win("me-player", "you-computer")
+print(response_win)
+
+
+
+
+
 
 def greeting():
     return "hi from the datacamp course"
 
 response = greeting()
-print(response)
+#print(response)
 
 choices = get_choices()
-print(choices)
+#print(choices)
+
+#lists
+food = ["piza", "carrots", "eggs"]
+dinner = random.choice(food)
