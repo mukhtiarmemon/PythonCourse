34c398951076db5f5c5b72f458a8469cbbbfd523
mukhtiarmemon <mukhtiar001@yahoo.com>
2024-10-24 11:52:22 +0500

    34fr

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 def get_choices():
     player_choice = "rock"
     computer_choice = "paper"
+    choices = {"player": player_choice, "computer": computer_choice}
+    
+    return choices
+
+def greeting():
+    return "hi from the datacamp course"
+
+response = greeting()
+print(response)
+
+choices = get_choices()
+print(choices)
+
+dict = {"name": "beau", "color": choices}
